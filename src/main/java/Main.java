ab268c253eff87030fdc24342ce303de89254be4
Eminolcer <72021999+Eminolcer@users.noreply.github.com>
2022-01-11 22:45:52 +0100

    Scanner function added, and system runs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         ArrayList<String> names = new ArrayList<>();
         names.add("Mette Frederiksen");
@@ -23,5 +26,53 @@ public class Main {
         names.add("Kaare Dybvad Bek");
         names.add("Magnus Johannes Heunicke");
         names.add("Lea  Wermelin");
+
+
+        Scanner sc = new Scanner(System.in);
+        int selection;
+        boolean exit = false;
+        boolean nameExist = false;
+        while (!exit) {
+            System.out.println("Menu : ");
+            System.out.println("Vælg mellem tallene 1 og 2, for at fortsætte");
+            System.out.println("1) Søg efter folk på listen med fulde navn");
+            System.out.println("2) Afslut");
+            selection = sc.nextInt();
+            switch (selection) {
+                case 1:
+                    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+                    System.out.println("Indtast fulde navn");
+                    String nameSearch = myObj.nextLine();  // Read user input
+
+                    for (int z = 0; z < names.size(); z++) {
+                        if (StringUtils.equalsIgnoreCase(nameSearch, names.get(z))) {
+                            System.out.println("Navnet findes på PEP-listen");
+
+                            nameExist = true;
+
+
+                            System.out.println("Vil du fortsætte? Indtast Ja / Nej");
+                            Scanner sc1 = new Scanner(System.in);
+                            if (sc1.next().equalsIgnoreCase("nej"))
+                                exit = true;
+                            break;
+                        }
+                    }
+
+                    if (!nameExist) {
+                        System.out.println("Dette navn findes ikke på PEP-Listen");
+                        System.out.println("Vil du fortsætte? Indtast Ja / Nej");
+                        Scanner sc1 = new Scanner(System.in);
+                        if (sc1.next().equalsIgnoreCase("nej"))
+                            exit = true;
+                        break;
+                    }
+
+
+                case 2:
+                    exit = true;
+                    break;
+            }
+        }
     }
 }
