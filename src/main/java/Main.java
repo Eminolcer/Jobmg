d006359e1f95ee4660ffe6c383d9d925aed3350a
Eminolcer <72021999+Eminolcer@users.noreply.github.com>
2022-01-11 22:15:29 +0100

    Array with names added

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Mette Frederiksen");
+        names.add("Jeppe Kofod");
+        names.add("Nick  Hækkerup");
+        names.add("Nicolai Halby Wammen");
+        names.add("Trine Bramsen");
+        names.add("Astrid Krag");
+        names.add("Flemming Møller Mortensen");
+        names.add("Peter  Hummelgaard");
+        names.add("Mattias Tesfaye");
+        names.add("Jesper Petersen");
+        names.add("Dan Jørgensen");
+        names.add("Morten Bødskov");
+        names.add("Pernille Rosenkrantz-Theil");
+        names.add("Ane Halsboe-Jørgensen");
+        names.add("Benny Engelbrecht");
+        names.add("Kaare Dybvad Bek");
+        names.add("Magnus Johannes Heunicke");
+        names.add("Lea  Wermelin");
+    }
+}
